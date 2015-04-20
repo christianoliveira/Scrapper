ce281ed6a1feb837af307fc2dd0ef0b5329fcf54
Christian Oliveira <christianoliveirausal@gmail.com>
2015-04-20 23:34:20 +0200

    Encapsulado en funciones
    Encapsulo los códigos en funciones para ir organizando y desaclopando
    las funcionalidades

--- scrapper.php
+++ scrapper.php
@@ -1,4 +1,15 @@
 <?php 
+
+	function getSERP($keyword, $country, $language, $googledomain){
+		$url = "http://www.".$googledomain."/search?q=".$keyword."&hl=".$language."&gl=".$country."&pws=0";
+		$html = file_get_contents($url);
+	}
+
+	function saveSerp($html, $keyword, $country, $language){
+		$fecha = New DateTime();
+		$fileName = $keyword."_".$language."_".$country."_".$fecha->getTimeStamp();
+		file_put_contents($fichero, $html);
+	}
 	/* Código básico que obtiene el html del resultado de búsqueda de la keyword definida */
 	$keyword = "alumno ballesta";
 	$keyword = str_replace(' ', '+', $keyword);
